241e395e8117ee37655cf985ef477a3b865f4a67
libby121 <58260595+libby121@users.noreply.github.com>
2025-04-02 20:49:15 +0300

    Update Role.java
    commited
    1

--- src/main/java/com/sourav/springsecurity/entity/Role.java
+++ src/main/java/com/sourav/springsecurity/entity/Role.java
@@ -20,4 +20,6 @@ public class Role {
     @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST) // save role with user
     @JoinColumn(name = "user_id", referencedColumnName = "userId") // Define the join column in the Role table
     private User user;
+    //wow
+    
 }
